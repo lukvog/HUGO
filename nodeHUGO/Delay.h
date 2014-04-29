aee77225b9f420363c963274bd31385c05d491bf
Lukas Truniger <lukas.truniger@gmail.com>
2014-04-29 21:35:29 +0200

    created the node, called nodeHugo and replaced the UltrasonicSensor with a spaceholder for the irSensor

---
+++ nodeHUGO/Delay.h
@@ -0,0 +1,102 @@
+/* 
+ * *** Delay.h ****
+ *
+ * created by Lukas Vogel 31.03.14
+ * 
+ */
+ 
+#ifndef Delay_h_
+#define Delay_h_
+
+#include "AudioStream.h"
+
+class DelayBuffer;
+
+class DelayTap
+{
+public:
+	DelayTap()
+	: mReadPtr(0),
+	mOldValue(0)
+	{}
+	~DelayTap() {}
+	void Read_WR(DelayBuffer& delayBuffer, uint32_t* outBuffer, int targetDelay, uint32_t bufferSize, int loopLength);
+	
+private:
+	int mReadPtr;
+	float mOldValue;
+};
+
+class DelayBuffer
+{
+
+friend class DelayTap;
+friend class Delay;
+
+public:
+	DelayBuffer(size_t maxDelay)
+	: mDelayLength(maxDelay),
+	mWritePtr(0)
+	{
+		mContent = new uint32_t[mDelayLength];
+		for(size_t i=0; i<mDelayLength; i++)
+		{
+			mContent[i] = 0;
+		}
+
+	}
+	~DelayBuffer()
+	{
+		delete[] mContent;
+	}
+	
+	void Write(uint32_t* inBuffer, uint32_t bufferSize, bool hold);
+	
+protected:
+	uint32_t* mContent;
+	size_t mDelayLength;
+	size_t mWritePtr;
+};
+
+class Delay : public AudioStream
+{
+public:
+	Delay() :
+	AudioStream(1,inputQueueArray),
+	mDelay(8192+AUDIO_BLOCK_SAMPLES),
+	targetDelay(0),
+	mHold(false),
+	loopLength(mDelay.mDelayLength)
+	{}
+	~Delay() {}
+
+	virtual void update(void);
+	void hold(bool hold) {
+		mHold = hold;
+	}
+	
+	void setTargetDelay(int _targetDelay)
+	{
+		__disable_irq();
+		targetDelay = _targetDelay;
+		__enable_irq();
+	}
+	
+		void setLoopLength(int _loopLength)
+	{
+		__disable_irq();
+		loopLength = _loopLength;
+		__enable_irq();
+	}
+	
+private:
+	audio_block_t *inputQueueArray[1];
+	DelayBuffer mDelay;
+	DelayTap myTap;
+	bool mHold;
+	int targetDelay;
+	int loopLength;
+};
+
+
+#endif
--- nodeHUGO/nodeHUGO.ino
+++ nodeHUGO/nodeHUGO.ino
@@ -39,33 +39,65 @@ PINS Audioboad:
 #include <RF24.h>
 #include <Bounce.h>
 #include "nodeconfig.h"
-#include "Ultrasonic.h"
 
+#include <Metro.h>
+
+
+#define MOD_RATE 5 // modulation rate in ms
+#define MOD_RATE_DIF 0.2
+
+#include "Delay.h"
+#include "FormantFilter.h"
+#include "Tone.h"
+#include "Sequences.h"
+
+#define Test1_PIN 1
+#define Test0_PIN 0
+
+Bounce b_test1 = Bounce(Test1_PIN,15);
+Bounce b_test0   = Bounce(Test0_PIN,15);
 
 
 //___________________________________________________________________________________
 /////////////////////////
-//AUDIO
+//AUDIO CONFIGURATION
 
 //const int myInput = AUDIO_INPUT_LINEIN;
 const int myInput = AUDIO_INPUT_MIC;
 
-// Create the Audio components.  These should be created in the
-// order data flows, inputs/sources -> processing -> outputs
-//
-//AudioInputAnalog analogPinInput(16); // analog A2 (pin 16)
-AudioInputI2S    audioInput;         // audio shield: mic or line-in
-AudioOutputI2S   audioOutput;        // audio shield: headphones & line-out
-AudioOutputPWM   pwmOutput;          // audio output with PWM on pins 3 & 4
+float delayVolume;
+
+AudioInputI2S       audioInput;         // audio shield: mic or line-in
+Delay 	staticDelay;
+AudioSynthWaveform 	osc1;
+AudioSynthWaveform 	osc2;
+AudioSynthWaveform	osc3;
+AudioMixer4			mixSources;
+AudioFilterBiquad    formantFilter1(ToneFilter1);
+AudioFilterBiquad    formantFilter2(ToneFilter2);
+AudioFilterBiquad    formantFilter3(ToneFilter3);
+AudioMixer4        mixFormants;
+AudioOutputI2S      audioOutput;        // audio shield: headphones & line-out
 
 // Create Audio connections between the components
-//
-AudioConnection c1(audioInput, 0, audioOutput, 0);
-AudioConnection c2(audioInput, 1, audioOutput, 1);
-AudioConnection c5(audioInput, 0, pwmOutput, 0);
+// Both channels of the audio input go to the FIR filter
+AudioConnection c1(audioInput, 0, staticDelay, 0);
+AudioConnection c2(staticDelay, 0, mixSources, 0);
 
-// Create an object to control the audio shield.
-// 
+AudioConnection c3(osc1, 0, mixSources, 1);
+AudioConnection c12(osc2, 0, mixSources, 2);
+AudioConnection c13(osc3, 0, mixSources, 3);
+AudioConnection c4(mixSources, 0, formantFilter1, 0);
+AudioConnection c5(mixSources, 0, formantFilter2, 0);
+AudioConnection c6(mixSources, 0, formantFilter3, 0);
+
+AudioConnection c7(mixSources, 0, mixFormants, 0);
+AudioConnection c8(formantFilter1, 0, mixFormants, 1);
+AudioConnection c9(formantFilter2, 0, mixFormants, 2);
+AudioConnection c10(formantFilter3, 0, mixFormants, 3);
+
+AudioConnection c11(mixFormants, 0, audioOutput, 0);
+
 AudioControlSGTL5000 audioShield;
 
 //___________________________________________________________________________________
@@ -137,7 +169,7 @@ void setup()
   Serial.begin(57600);
 
   //___________________________________________________________________________________
-  //Ultrasonic
+  //Ultrasonic aka  RF
 
   //calibration for Walls
 
@@ -176,12 +208,54 @@ void setup()
   //___________________________________________________________________________________
   // Audio connections require memory to work.  For more
   // detailed information, see the MemoryAndCpuUsage example
-  AudioMemory(12);
+  delay(3000);
 
-  // Enable the audio shield and set the output volume.
-  audioShield.enable();
-  audioShield.inputSelect(myInput);
-  audioShield.volume(60);
+	pinMode(Test1_PIN,INPUT_PULLUP);
+	pinMode(Test0_PIN,INPUT_PULLUP);
+
+	// It doesn't work properly with any less than 8
+	AudioMemory(12);
+
+	audioShield.enable();
+	audioShield.inputSelect(myInput);
+	audioShield.volume(90);
+	
+	mixFormants.gain(0, 0.0001);
+	mixFormants.gain(1, 0.4);
+	mixFormants.gain(2, 0.4);
+	mixFormants.gain(3, 0.4);
+	
+	delayVolume = 2;
+	
+	mixSources.gain(0, delayVolume);
+	mixSources.gain(1, 0.1);
+	mixSources.gain(2, 0.1);
+	mixSources.gain(3, 0.1);
+	
+	setSopranA();
+	
+	osc1.set_ramp_length(88);
+	osc2.set_ramp_length(88);
+	osc3.set_ramp_length(88);
+	
+	masterFormantSeq.push_back(formantSeq1);
+	masterFormantSeq.push_back(formantSeq2);
+	masterToneSeq.push_back(toneSeq1_1);
+	masterToneSeq.push_back(toneSeq1_2);
+	masterToneSeq.push_back(toneSeq1_3);
+	masterToneSeq.push_back(toneSeq2_1);
+	masterToneSeq.push_back(toneSeq2_2);
+	masterToneSeq.push_back(toneSeq2_3);
+	masterDelayStateSeq.push_back(delayStateSeq1);
+	masterDelayStateSeq.push_back(delayStateSeq2);
+	masterToneVolSeq.push_back(toneVolumeSeq1);
+	masterToneVolSeq.push_back(toneVolumeSeq2);
+	
+	// osc1.begin(0.1,tune_frequencies2_PGM[30], TONE_TYPE_SQUARE);
+	// osc2.begin(0.1,tune_frequencies2_PGM[37], TONE_TYPE_SQUARE);
+	// osc3.begin(0.1,tune_frequencies2_PGM[44], TONE_TYPE_SQUARE);
+	
+	Serial.println("audio setup done");
 
   //___________________________________________________________________________________
   //RF24
@@ -219,6 +293,8 @@ void setup()
 
 }
 
+//___________________________________________________________
+
 //Metros
 elapsedMillis usageSec=0;
 elapsedMillis comSec=0;
@@ -229,7 +305,17 @@ elapsedMillis writeSec=0;
 elapsedMillis volSec=0;
 
 // audio volume
-int volume = 0;
+int mainVolume = 0;
+int oldValue = 0;
+int actFromSeq = 0;
+int activeToneSeq = 0;
+int actDelStateSeq = 0;
+int actOscVolSeq = 0;
+
+Metro MonitorMetro = Metro(1000);
+Metro ReadMetro = Metro(10);
+Metro TimingMetro = Metro(MOD_RATE);
+
 
 void loop() {
 
@@ -278,14 +364,132 @@ void loop() {
   //AUDIO
   // volume control
   // every 10 ms, check for adjustment
-  if (volSec > 500) {
-    int n = analogRead(15);
-    if (n != volume) {
-      volume = n;
-      audioShield.volume((float)n / 10.23);
-    }
-    volSec=0;
-  }
+  // volume control
+  
+	// every 10 ms, check for adjustment
+	if (ReadMetro.check() == 1) {
+		int n = analogRead(15);
+		if (n != mainVolume) {
+			mainVolume = n;
+			audioShield.volume((float)n / 10.23);
+		}
+	}
+	
+	if (TimingMetro.check() == 1) {
+		
+		// formant filter sequence
+		if (masterFormantSeq[actFromSeq].seqCounter < masterFormantSeq[actFromSeq].seqLength)
+		{		
+			masterFormantSeq[actFromSeq].seqProceed();
+		}
+		else
+		{
+			if (masterFormantSeq[actFromSeq].loop == true)
+			{
+				masterFormantSeq[actFromSeq].reset();
+				masterFormantSeq[actFromSeq].seqProceed();
+			}			
+		}
+				
+		// osc1 sequence
+		if (masterToneSeq[activeToneSeq].seqCounter < masterToneSeq[activeToneSeq].seqLength)
+		{			
+			masterToneSeq[activeToneSeq].seqProceed();
+		}
+		else
+		{
+			if (masterToneSeq[activeToneSeq].loop == true)
+			{
+				masterToneSeq[activeToneSeq].reset();
+				masterToneSeq[activeToneSeq].seqProceed();
+			}			
+		}
+		
+		// osc2 sequence
+		if (masterToneSeq[activeToneSeq+1].seqCounter < masterToneSeq[activeToneSeq+1].seqLength)
+		{			
+			masterToneSeq[activeToneSeq+1].seqProceed();
+		}
+		else
+		{
+			if (masterToneSeq[activeToneSeq+1].loop == true)
+			{
+				masterToneSeq[activeToneSeq+1].reset();
+				masterToneSeq[activeToneSeq+1].seqProceed();
+			}			
+		}
+		
+		// osc3 sequence
+		if (masterToneSeq[activeToneSeq+2].seqCounter < masterToneSeq[activeToneSeq+2].seqLength)
+		{			
+			masterToneSeq[activeToneSeq+2].seqProceed();
+		}
+		else
+		{
+			if (masterToneSeq[activeToneSeq+2].loop == true)
+			{
+				masterToneSeq[activeToneSeq+2].reset();
+				masterToneSeq[activeToneSeq+2].seqProceed();
+			}			
+		}
+		
+		// delay state sequence
+		if (masterDelayStateSeq[actDelStateSeq].seqCounter < masterDelayStateSeq[actDelStateSeq].seqLength)
+		{	
+			masterDelayStateSeq[actDelStateSeq].seqProceed();
+		}
+		else
+		{
+			if (masterDelayStateSeq[actDelStateSeq].loop == true)
+			{
+				masterDelayStateSeq[actDelStateSeq].reset();
+				masterDelayStateSeq[actDelStateSeq].seqProceed();
+			}			
+		}
+		
+		
+		// tone volume sequence
+		if (masterToneVolSeq[actOscVolSeq].seqCounter < masterToneVolSeq[actOscVolSeq].seqLength)
+		{	
+			masterToneVolSeq[actOscVolSeq].seqProceed();
+		}
+		else
+		{
+			if (masterToneVolSeq[actOscVolSeq].loop == true)
+			{
+				masterToneVolSeq[actOscVolSeq].reset();
+				masterToneVolSeq[actOscVolSeq].seqProceed();
+			}			
+		}
+
+		
+	}
+	
+	// update the two buttons
+	b_test0.update();
+	b_test1.update();
+
+
+	// if pin1 is grounded
+	if(b_test1.fallingEdge()) {
+		//staticDelay.setLoopLength(AUDIO_BLOCK_SAMPLES * 32);
+	}
+
+	// If pin1 is open
+	if(b_test1.risingEdge()) {
+		//staticDelay.setLoopLength(AUDIO_BLOCK_SAMPLES * 64);
+	}
+
+	// if pin0 is grounded
+	if(b_test0.fallingEdge()) {
+		//staticDelay.hold(false);	
+		//actFromSeq = 1;
+	}
+	// if pin 0 is open
+	if(b_test0.risingEdge()) {
+		//staticDelay.hold(true);	
+		//actFromSeq = 0;
+	}
 
   //////////////////
   //Mem and CPU Usage
